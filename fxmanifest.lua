38f9ad02acdb903a21c76f41f4b45473ffda436c
Tizas <141794006+Tiziukas@users.noreply.github.com>
2025-01-30 11:07:51 +0000

    feat(esx_drugs/fxmanifest): Use lazy loading for locales.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,24 +3,26 @@ fx_version 'adamant'
 game 'gta5'
 lua54 'yes'
 description 'Allows players to harvest and sell marijuana'
-version '1.0.1'
+version '1.0.2'
 
-shared_script '@es_extended/imports.lua'
+shared_scripts {
+    '@es_extended/imports.lua',
+    '@es_extended/locale.lua',
+    'config.lua'
+}
 
 server_scripts {
 	'@oxmysql/lib/MySQL.lua',
-	'@es_extended/locale.lua',
-	'locales/*.lua',
-	'config.lua',
 	'server/main.lua'
 }
 
 client_scripts {
-	'@es_extended/locale.lua',
-	'locales/*.lua',
-	'config.lua',
 	'client/main.lua',
 	'client/weed.lua'
+}
+
+files {
+	'locales/*.lua',
 }
 
 dependencies {
